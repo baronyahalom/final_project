31df150a999fbdd2da9543d385f7c95e6f0f1e50
baronyahalom <yahalombaron@gmail.com>
2020-05-30 22:19:41 +0300

    yesssssssssss

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  url: "mongodb://localhost:27017/hex_game"
+  url: "mongodb+srv://yahalom:<password>@cluster0-qnq1f.mongodb.net/test?retryWrites=true&w=majority",
+  secret: 'yoursecret'
 };
