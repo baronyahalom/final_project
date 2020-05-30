7a998f19773e9cf4d24eff5eac273f6ad0975f35
baronyahalom <yahalombaron@gmail.com>
2020-05-30 22:42:24 +0300

    hh

--- server.js
+++ server.js
@@ -32,15 +32,21 @@ app.listen(PORT, () => {
 
 
  const db = require("./app/models");
- db.mongoose
-   .connect(db.url, {
-     useNewUrlParser: true,
-     useUnifiedTopology: true
-   })
-   .then(() => {
-     console.log("Connected to the database!");
-   })
-   .catch(err => {
-     console.log("Cannot connect to the database!", err);
-     process.exit();
-   });
+//  db.mongoose
+//    .connect(db.url, {
+//      useNewUrlParser: true,
+//      useUnifiedTopology: true
+//    })
+//    .then(() => {
+//      console.log("Connected to the database!");
+//    })
+//    .catch(err => {
+//      console.log("Cannot connect to the database!", err);
+//      process.exit();
+//    });
+
+   db.mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+   const conn = mongoose.connection;
+   mongoose.connection.once('open', () => { console.log('MongoDB Connected'); });
+   mongoose.connection.on('error', (err) => { console.log('MongoDB connection error: ', err); }); 
+   
