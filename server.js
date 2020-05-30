e517c4a06d45c944f94f3d0367fef68b4589587e
baronyahalom <yahalombaron@gmail.com>
2020-05-30 23:08:43 +0300

    g

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 
 // simple route
@@ -32,7 +35,6 @@ app.listen(PORT, () => {
 
 
 const db = process.env.MONGODB_URL;
-
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
@@ -52,3 +54,18 @@ corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
+
+
+// const db = require("./app/models");
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch(err => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
