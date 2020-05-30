dfef0b95649cb1fd2329dd09babcbc22c0840717
baronyahalom <yahalombaron@gmail.com>
2020-05-30 22:26:55 +0300

    'pass'

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  url: "mongodb+srv://yahalom:<password>@cluster0-qnq1f.mongodb.net/test?retryWrites=true&w=majority",
+  url: "mongodb+srv://yahalom:im21DHhbMhSB5uD8@cluster0-qnq1f.mongodb.net/test?retryWrites=true&w=majority",
   secret: 'yoursecret'
 };
